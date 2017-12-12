f684b88b22ecbc1843f8f285711fe162cfb82b4c
Marija <axabuci@gmail.com>
2017-12-12 10:23:26 +0100

    changed Receipt

--- Receipt.cpp
+++ Receipt.cpp
@@ -1,24 +1,66 @@
 #include "Receipt.h"
+using std::string;
+using std::vector;
 
 
-
-Receipt::Receipt(std::string name, std::string key, double sold, double price, double total, double pdv) :pdv(pdv)
+Receipt::Receipt(std::string name, double total, double pdv) :pdv(pdv)
 {
 	//Date
 	buyers_name = name;
-	product_key = key;
-	product_sold = sold;
-	product_price = price;
+
 	total_price = total;
 }
-
+Receipt::Receipt(string name, string buyer, vector<string> products, vector<double> prices,
+	vector<double> amount, double total, double pdv) :Receipt(buyer, total, pdv)
+{
+	receipts_name = name;
+	product_key = products;
+	product_price = prices;
+	product_sold = amount;
+}
 Receipt format(const char* file)  //not finished
 {
 
-	std::string file_name = "dadasf", buyers_name;
+	std::string file_name = file, buyers_name;
 	double sold, price, pdv;
 	//Date date;
-	Receipt r(file_name, file_name, 0.0, 0.0, 0.0, 0.0);
+	char temp;
+	std::string skip, take, name;
+	std::ifstream myFile;
+	myFile.open(file);
+	//need to check if file is empty
+	myFile >> skip;
+	if (skip != "Kupac:")
+		return Receipt::null_receipt;
+	myFile >> name;
+	myFile.get(temp);
+	if (temp != '\n')
+		return Receipt::null_receipt;
+	myFile >> skip;
+	if (skip != "Datum:")
+		return Receipt::null_receipt;
+	myFile.get(temp);
+	myFile >> skip;//Date
+				   //check Date
+	myFile.get(temp);
+	if (temp != '\n')
+		return Receipt::null_receipt;
+	myFile.get(temp);
+	if (temp != '\n')
+		return Receipt::null_receipt;
+	char c[100];
+	myFile.getline(c, 30);
+	if (strcmp(c, "\t       Racun") != 0)
+		return Receipt::null_receipt;
+	myFile.get(temp);
+	if (temp != '\n')
+		return Receipt::null_receipt;
+	myFile.getline(c, 80);
+	if (strcmp(c, "Proizvod - kolicina - cijena - ukupno") != 0)
+		return Receipt::null_receipt;
+
+	Receipt r(file_name, 0.0, 0.0);
 	return r;
 }
-Receipt Receipt::null_receipt("", "", 0.0, 0.0, 0.0, 0.0);
+
+Receipt Receipt::null_receipt("", 0.0, 0.0);
--- Receipt.h
+++ Receipt.h
@@ -2,22 +2,26 @@
 #include<string>
 #include<iostream>
 #include<fstream>
+#include<vector>
 class Receipt
 {
 public:
-	Receipt(std::string, std::string, double, double, double, double);
+	Receipt(std::string, double, double);
+	Receipt(std::string, std::string, std::vector<std::string>, std::vector<double>, std::vector<double>, double, double);
 	Receipt format(const char*);///not finished
 	Receipt(const Receipt&) = default;
 	Receipt& operator=(const Receipt&) = default;
 private:
 	std::string buyers_name;
+	std::string receipts_name;
 	//Date date;
-	std::string product_key;
-	double product_sold;
-	double product_price;
+	std::vector<std::string> product_key;
+	std::vector<double> product_sold;
+	std::vector<double> product_price;
 	double total_price;
 	double pdv;
 public:
 	static Receipt null_receipt;
 };
+
 
