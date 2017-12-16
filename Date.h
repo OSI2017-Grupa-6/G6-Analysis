f5140d82a165410a2e234c839f7f2e9562d16e0b
TatjanaTomic <34337951+TatjanaTomic@users.noreply.github.com>
2017-12-16 02:00:36 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -6,10 +6,7 @@ class Date
 	
 public:
 	Date(int, int, int);
-	int get_day() { return day; }
-	int get_month() { return month; }
-	int get_year() { return year; }
-	bool check_date(int, int, int);
+	bool check_date();
 	operator string() const;
 	Date string_to_date(string);
 	~Date();
