ef3c94422a7a0b1e76a5ed93f13170f7bd2f3c5f
TatjanaTomic <34337951+TatjanaTomic@users.noreply.github.com>
2017-12-16 01:59:38 +0100

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -13,25 +13,18 @@ Date::Date(int d, int m , int y) : day(d), month(m), year(y)
 
 bool Date::check_date()
 {
-		if (!(year <= MIN_YEAR && year >= MAX_YEAR))
+		if (year < MIN_YEAR || year > MAX_YEAR)
 			return false;
-		if (!(1 <= month && month <= 12))
+		if (month < 1 || month > 12))
 			return false;
-		if (!(1 <= day && day <= 31))
+		if (day < 1 || day > 31))
 			return false;
 		if ((day == 31) && (month == 2 || month == 4 || month == 6 || month == 9 || month == 11))
 			return false;
 		if ((day == 30) && (month == 2))
 			return false;
-		if ((month == 2) && (day == 29) && (year % 4 != 0))
+		if ((month == 2) && (day == 29) && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
 			return false;
-		if ((month == 2) && (day == 29) && (year % 400 == 0))
-			return true;
-		if ((month == 2) && (day == 29) && (year % 100 == 0))
-			return false;
-		if ((month == 2) && (day == 29) && (year % 4 == 0))
-			return true;
-
 		return true;
 }
 
