dae76b512315de9aed2eca93bbf0e6b16f4aabdc
TatjanaTomic <34337951+TatjanaTomic@users.noreply.github.com>
2017-12-17 23:12:04 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include"Processing.h"
 #include"Administrator.h"
 #include"Analyst.h"
@@ -8,63 +7,27 @@
 #include<vector>
 #define LOGOUT 0
 #define EXIT -99
-int main()
+
+int main(int argc, char**argv)
 {
-#include"Processing.h"
-#include"Administrator.h"
-#include"Analyst.h"
-#include"UsersGroup.h"
-
-#include<iostream>
-#include<vector>
-#define LOGOUT 0
-#define EXIT -99
-	int main()
+	if (argc == 1)
 	{
-		//processing("Racuni","C:\\Users\\PWIN\\Desktop\\Programski jezici 1\\Projekti\\Project2\\Project2\\");
-		int response;
-		int system_on;
-		Administrator a; //example of first user
-		a.addAccount();
-		do {
-			std::cout << std::endl << "Do you want to login:" << std::endl;
-			std::cin >> response;
-			if (response)
-			{
-				Administrator admin;
-				Analyst analyst;
-				UsersGroup user;
-				user.login();
-				int group = user.getGroup();
-
-				if (group == USER_ADMIN) {
-					admin.cast(user);
-				}
-				else analyst.cast(user);
-				if (group == USER_ADMIN)
-					admin.options();
-				std::cout << group;
-				if (group == USER_ANALYST)
-					analyst.options();
-			}
-			std::cout << std::endl << "EXIT? (-99)" << std::endl;
-			std::cin >> system_on;
-		} while (system_on != EXIT);
+		std::cout << "Wrong path!!!";
 	}
-	//processing("Racuni","C:\\Users\\PWIN\\Desktop\\Programski jezici 1\\Projekti\\Project2\\Project2\\");
+	processing("Racuni", argv[1]);//begin processing
 	int response;
 	int system_on;
-	//Administrator a;
-	//a.addAccount();
+	std::cout << "WELCOME TO THE SYSTEM!!!" << std::endl << std::endl;
 	do {
-		std::cout <<std::endl<< "Do you want to login:" << std::endl;
+		std::cout <<std::endl<< "Do you want to login? YES(1)/NO(0)" << std::endl;
 		std::cin >> response;
 		if (response)
 		{
 			Administrator admin;
 			Analyst analyst;
 			UsersGroup user;
-			user.login();
+			if (user.login() == LOGOUT)
+				break;
 			int group = user.getGroup();
 			
 			if (group == USER_ADMIN) {
@@ -73,11 +36,11 @@ int main()
 			else analyst.cast(user);
 			if (group == USER_ADMIN)
 				admin.options();
-			std::cout << group;
+
 			if (group == USER_ANALYST)
 				analyst.options();
 		}
-		std::cout << std::endl<<"EXIT? (-99)"<<std::endl;
+		std::cout << std::endl<<"EXIT? YES(-99)/NO(0)"<<std::endl;
 		std::cin >> system_on;
 	} while (system_on != EXIT);
 }
