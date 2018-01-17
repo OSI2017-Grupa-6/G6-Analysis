8acc0fbb4d967750d621edb2e1b409bbaf370a52
TatjanaTomic <34337951+TatjanaTomic@users.noreply.github.com>
2018-01-18 00:22:36 +0100

    Add files via upload

---
+++ Currency.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void set_currency();
+
+void check_currency()
+{
+	int length = 0;
+	std::ifstream file("Valuta.txt", std::ifstream::binary);
+	if (file)
+	{
+		file.seekg(0, file.end);
+		length = file.tellg();
+		file.close();
+	}
+	if (length == 0)
+		set_currency();
+
+}
+
+void set_currency()
+{
+	bool flag = false;
+	std::string currency;
+	do {
+		std::cout << "Izaberite valutu sistema!\n\nMoguce opcije su:\n\tKM (konvertibilna marka)\n\t€ (euro)" << std::endl;
+		std::cin >> currency;
+		if ((currency.compare("KM") == 0 || currency.compare("€") == 0))
+			flag = true;
+	} while (!flag);
+	std::ofstream file;
+	file.open("Valuta.txt");
+	if (file)
+		file << currency;
+	file.close();
+}
