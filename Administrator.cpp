e958d0725af10000ad82004319597e0fcd0877ba
Marija <axabuci@gmail.com>
2018-01-19 20:22:40 +0100

    Added update of account

--- Administrator.cpp
+++ Administrator.cpp
@@ -1,7 +1,7 @@
 #include "Administrator.h"
 #include"Currency.h"
 #include<fstream>
-#include "Load.h"
+
 using std::string;
 using std::cout;
 using std::cin;
@@ -13,40 +13,11 @@ Administrator::Administrator(string name, string lastName, string pin, int userg
 	UsersGroup(name, lastName, pin, usergroup, username)
 {}
 
-bool Administrator::addAccount(int first_acc)
+bool Administrator::addAccount()
 {
 	std::vector<string> vec;
-	string temp;
-	cout << "Korisnicko ime:";    cin >> temp;
-	vec.push_back(temp);
-	
-	temp = looking(vec, "Korisnici.txt");
-	if (temp == "") {
-		cout << "Nalog vec postoji!"<<std::endl; return false;
-	}
-
-	do {
-		cout << endl << "PIN(4 broja):";
-		cin >> temp;
-	} while (temp.size() != 4);
-	vec.push_back(temp);
-
-	cout << endl << "Ime:";                 cin >> temp;
-	vec.push_back(temp);
-	cout << endl << "Prezime:";    cin >> temp;
-	vec.push_back(temp);
-	int ug;
-	if (first_acc == 0)
-		do {
-			cout << endl << "Korisnicka grupa(administrator(-1) / analiticar(-2)):";
-			ug = input_int(std::cin);
-			if (ug != USER_ADMIN && ug != USER_ANALYST)
-				std::cout << "Ne postojeca opcija, pokusajte ponovo:";
-		} while (ug != USER_ADMIN && ug != USER_ANALYST);
-	else ug = -1;      // first account  must be admin
-	cout << endl;
-	string tmp = std::to_string(ug);
-	std::string check = "UN:" + vec[0] + "NA:" + vec[2] + "LN:" + vec[3] + "P:" + vec[1] + "UG:" + tmp;
+	string check;
+	adding_info(vec, check);
 	std::ofstream dat("Korisnici.txt", std::ios::app);
 	dat << check << endl;
 	dat.close();
@@ -77,7 +48,7 @@ bool Administrator::deleteAccount()
 		file.seekp(location);
 		file << "\n";
 		for (int i = 1; i < size-1; ++i)
-			file << '0';
+			file << '^';
 		file << "\n";
 		file.close();
 		return true;
@@ -101,7 +72,7 @@ bool Administrator::getListOfUsers()
 		line = _line;
 		user_information(vec, line);
 		
-		if (vec[0][0] != '0') {
+		if (vec[0][0] != '^') {
 			if (line != "") {
 				std::cout << ++i << ".korisnik:" << std::endl;
 				UsersGroup u(vec[2], vec[3], vec[0], std::stoi(vec[4]), vec[1]);
@@ -109,6 +80,54 @@ bool Administrator::getListOfUsers()
 			}
 		}
 	}
+	file.close();
+	return true;
+}
+
+bool Administrator::updateAccount()
+{
+	std::vector<string> vec;
+	string temp;
+	for(int i=0;i<ACCOUNT_SIZE;++i)
+	vec.push_back("");
+	std::cout << "Informacije o korisniku kojeg zelite da azurirate:" << std::endl;
+	std::cout << "Korisnicko ime:" << std::endl;
+	std::cin >> vec[0]; 
+	std::cout << "Pin:" << std::endl;
+	std::cin >> vec[1]; 
+	int location;
+	int size;
+	looking_for_delete(vec, "Korisnici.txt", &location, &size);
+	if (location == -1)
+	{
+		std::cout << "\nKorisnik nije pronadjen.\n";
+		return false;
+	}
+	std::cout << "\nKorisnik je pronadjen.\n";
+	std::ofstream file;
+	file.open("Korisnici.txt", std::ios::in);
+	file.seekp(location);
+	file << "\n";
+	for (int i = 1; i < size - 1; ++i)
+		file << '^';
+	file << "\n";
+	file.close();
+	file.open("Korisnici.txt", std::ios::in);
+	vec.clear();
+	std::cout << "Unesite nove informacije:";
+	std::string check;
+	adding_info(vec, check);
+	int new_size = check.size() + 1;
+	file.seekp(location);
+	file << "\n";
+	file << check;
+	while (new_size < size-1) {
+		file << ' ';
+		new_size++;
+		location = 3;
+	}
+
+    file << "\n";
 	file.close();
 	return true;
 }
@@ -121,18 +140,18 @@ int Administrator::options()
 	do {
 		do {
 			std::cout << "\nOpcija (1): Dodaj nalog\nOpcija (2): Pregled liste korisnika\nOpcija (3): Brisanje naloga" << std::endl;
-			answer=input_int(std::cin);
-			if (answer != 0 && answer != 1 && answer != 2 && answer != 3)
-				std::cout << "Ne postojeca opcija, pokusajte ponovo:" << std::endl;
-		} while (answer != 0 && answer != 1 && answer!=2 && answer!=3);
+			std::cout << "Opcija (4): Azuriranje naloga\n";
+			std::cin >>answer;
+		} while (answer != 0 && answer != 1 && answer!=2 && answer!=3 && answer!=4);
 		
 		switch (answer)
 		{
 		case(1): addAccount(); break;
 		case(2):getListOfUsers() ? std::cout<<"" : std::cout << "Postojeca lista korisnika je prazna."; break;
 		case(3):deleteAccount() ? std::cout<<"\nUspjesno ste obrisali nalog." : std::cout << "\nNeuspjeno brisanje.\n"; break;
+		case(4):updateAccount(); break;
 		}
-	} while (answer==1 || answer==2 || answer==3);
+	} while (answer==1 || answer==2 || answer==3 || answer==4);
 	return LOGOUT;
 }
 
@@ -183,5 +202,42 @@ void Administrator::looking_for_delete(std::vector<std::string>& vec, const char
 	}
 	dat.close();
 	*location = -1;
+}
+
+bool Administrator::adding_info(std::vector<std::string>& vec, std::string & check) const
+{
+	std::string temp;
+	bool flag = false;
+	do {
+		cout << "Korisnicko ime:";    cin >> temp;
+		if (!flag)
+			vec.push_back(temp);
+		else
+			vec[0] = temp;
+		temp = looking(vec, "Korisnici.txt");
+		if (temp == "") {
+			cout << "Nalog vec postoji!" << std::endl;
+			flag = true;
+		}
+	} while (temp == "");
+	do {
+		cout << endl << "PIN(4 broja):";
+		cin >> temp;
+	} while (temp.size() != 4);
+	vec.push_back(temp);
+
+	cout << endl << "Ime:";                 cin >> temp;
+	vec.push_back(temp);
+	cout << endl << "Prezime:";    cin >> temp;
+	vec.push_back(temp);
+	int ug;
+		do {
+			cout << endl << "Korisnicka grupa(administrator(-1) / analiticar(-2)):";
+			cin >> ug;
+		} while (ug != USER_ADMIN && ug != USER_ANALYST);
+	cout << endl;
+	string tmp = std::to_string(ug);
+	check = "UN:" + vec[0] + "NA:" + vec[2] + "LN:" + vec[3] + "P:" + vec[1] + "UG:" + tmp;
+	return true;
 }
 
--- Administrator.h
+++ Administrator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<string>
+#include<vector>
 #include<iostream>
 
 #include"UsersGroup.h"
@@ -10,12 +11,14 @@ class Administrator :public UsersGroup
 public:
 	Administrator() = default;
 	Administrator(std::string, std::string, std::string, int, std::string);
-	bool addAccount(int first_acc=0);
+	bool addAccount();
 	bool deleteAccount();
 	bool getListOfUsers();
+	bool updateAccount();
 	int options();
 protected:
 	std::string looking(std::vector<std::string>&, const char* file) const override;
 	void looking_for_delete(std::vector<std::string>&, const char* file, int* location,int* size) const;
+	bool adding_info(std::vector<std::string>& vec, std::string& check) const;
 };
 
