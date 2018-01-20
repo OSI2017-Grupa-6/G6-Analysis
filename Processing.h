f04ecded696f9ea79789e1139fb5cbd634df04e7
Marija <axabuci@gmail.com>
2018-01-20 08:43:30 +0100

    Nothing special

--- Processing.h
+++ Processing.h
@@ -1,11 +1,7 @@
 #pragma once
 #include"Receipt.h"
 #include"Load.h"
-#include <string>
-
 
 void processing(const char*,const char*);
-void send_to_files(std::vector<Receipt>&,const char*);
-void send_to_files_month(std::vector<Receipt>, const char*);
+void send_to_files(std::vector<Receipt>&,const char*path);
 bool is_empty(const char*);
-std::string get_month(std::string);
